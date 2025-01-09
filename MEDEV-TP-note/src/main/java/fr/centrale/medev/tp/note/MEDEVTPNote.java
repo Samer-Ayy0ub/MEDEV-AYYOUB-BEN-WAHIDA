e992c584994008e1590fd90282ec04dd954c9271
samer <sameroayyoub@gmail.com>
2025-01-09 16:49:49 +0100

    create main class

---
+++ MEDEV-TP-note/src/main/java/fr/centrale/medev/tp/note/MEDEVTPNote.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fr.centrale.medev.tp.note;
+
+/**
+ *
+ * @author samer
+ */
+public class MEDEVTPNote {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
