d183260aab0d931c0817b24e9b7e1891252d45cd
Nadhem031 <Nadhem.Ben-Wahida@eleves.ec-nantes.fr>
2025-01-13 09:21:19 +0100

    Ajout des attributs et constructeur de la classe Board

--- MEDEV-TP-note/src/main/java/fr/centrale/medev/tp/note/Board.java
+++ MEDEV-TP-note/src/main/java/fr/centrale/medev/tp/note/Board.java
@@ -8,6 +8,34 @@ package fr.centrale.medev.tp.note;
  *
  * @author Nadhem
  */
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board {
-    
+    private final Cell[][] grid;
+
+    // Dimensions du plateau
+    private static final int SIZE = 8;
+
+    // Constructeur
+    public Board() {
+        grid = new Cell[SIZE][SIZE];
+        initializeBoard();
+    }
+     // Initialisation du plateau
+    private void initializeBoard() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                grid[i][j] = new Cell(i, j); // Chaque cellule connaît ses coordonnées
+            }
+        }
+
+        // Placement initial des pions au centre du plateau
+        grid[3][3].setState('B');
+        grid[4][4].setState('B');
+        grid[3][4].setState('N');
+        grid[4][3].setState('N');
+    }
+
 }
