fe08d5f6c8f5f469fdf32e60249dac6a99643bea
Nadhem031 <Nadhem.Ben-Wahida@eleves.ec-nantes.fr>
2025-01-13 08:57:17 +0100

    Ajout des méthodes dans la classe cell

--- MEDEV-TP-note/src/main/java/fr/centrale/medev/tp/note/Cell.java
+++ MEDEV-TP-note/src/main/java/fr/centrale/medev/tp/note/Cell.java
@@ -9,5 +9,32 @@ package fr.centrale.medev.tp.note;
  * @author Nadhem
  */
 public class Cell {
+    private int x; // Position X de la cellule
+    private int y; // Position Y de la cellule
+    private char state; // 'N' pour Noir, 'B' pour Blanc, ' ' pour vide
     
+    // Getter pour X
+    public int getX() {
+        return x;
+    }
+
+    // Getter pour Y
+    public int getY() {
+        return y;
+    }
+
+    // Getter pour l'état
+    public char getState() {
+        return state;
+    }
+
+    // Setter pour l'état
+    public void setState(char state) {
+        this.state = state;
+    }
+    // Représentation en chaîne de caractères (pour affichage)
+    @Override
+    public String toString() {
+        return String.valueOf(state);
+    }
 }
