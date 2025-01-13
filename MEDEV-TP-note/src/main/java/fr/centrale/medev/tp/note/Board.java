398b6d1483ae2538ece2c69222955af85c9a30fd
samer <sameroayyoub@gmail.com>
2025-01-13 09:29:05 +0100

    add Board again

---
+++ MEDEV-TP-note/src/main/java/fr/centrale/medev/tp/note/Board.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.centrale.medev.tp.note;
+
+/**
+ *
+ * @author samer
+ */
+class Board {
+
+    void makeMove(int x, int y, Player aThis) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
